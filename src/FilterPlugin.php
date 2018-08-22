0fde03088790afc5e0a1ecfe747967ca07e420fa
Angel Genchev <angel.d.genchev@gmail.com>
2018-08-22 11:22:14 +0300

    composer fixed the style

--- src/FilterPlugin.php
+++ src/FilterPlugin.php
@@ -3,8 +3,8 @@
 namespace Openbuildings\Swiftmailer;
 
 use Openbuildings\Swiftmailer\Filters\FilterInterface;
-use Swift_Events_SendListener;
 use Swift_Events_SendEvent;
+use Swift_Events_SendListener;
 
 /**
  * @author     Ivan Kerin <ikerin@gmail.com>
@@ -16,77 +16,77 @@ class FilterPlugin implements Swift_Events_SendListener
     /**
      * @var FilterInterface[]
      */
-	private $filters;
+    private $filters;
 
-	public function __construct(array $filters = [])
-	{
-		$this->filters = $filters;
-	}
+    public function __construct(array $filters = [])
+    {
+        $this->filters = $filters;
+    }
 
-	/**
-	 * @param  string $email
-	 * @return boolean
-	 */
-	public function filterEmail($email)
-	{
-	    foreach ($this->filters as $filter) {
-	        $shouldKeepEmail = $filter->checkEmail($email);
+    /**
+     * @param string $email
+     *
+     * @return bool
+     */
+    public function filterEmail($email)
+    {
+        foreach ($this->filters as $filter) {
+            $shouldKeepEmail = $filter->checkEmail($email);
             if (!$shouldKeepEmail) {
                 return true;
             }
         }
 
         return false;
-	}
+    }
 
-	/**
-	 * @param  array  $emails
-	 * @return array
-	 */
-	public function filterEmailArray(array $emails)
-	{
-		foreach ($emails as $email => $name) {
-			if ($this->filterEmail($email)) {
-				unset($emails[$email]);
-			}
-		}
+    /**
+     * @param array $emails
+     *
+     * @return array
+     */
+    public function filterEmailArray(array $emails)
+    {
+        foreach ($emails as $email => $name) {
+            if ($this->filterEmail($email)) {
+                unset($emails[$email]);
+            }
+        }
 
-		return $emails;
-	}
+        return $emails;
+    }
 
-	/**
-	 * Apply whitelist and blacklist to "to", "cc" and "bcc"
-	 *
-	 * @param Swift_Events_SendEvent $event
-	 */
-	public function beforeSendPerformed(Swift_Events_SendEvent $event)
-	{
-		$message = $event->getMessage();
+    /**
+     * Apply whitelist and blacklist to "to", "cc" and "bcc".
+     *
+     * @param Swift_Events_SendEvent $event
+     */
+    public function beforeSendPerformed(Swift_Events_SendEvent $event)
+    {
+        $message = $event->getMessage();
 
-		$to = $this->filterEmailArray((array) $message->getTo());
-		$cc = $this->filterEmailArray((array) $message->getCc());
-		$bcc = $this->filterEmailArray((array) $message->getBcc());
+        $to = $this->filterEmailArray((array) $message->getTo());
+        $cc = $this->filterEmailArray((array) $message->getCc());
+        $bcc = $this->filterEmailArray((array) $message->getBcc());
 
-		$message->setTo($to);
-		$message->setCc($cc);
-		$message->setBcc($bcc);
+        $message->setTo($to);
+        $message->setCc($cc);
+        $message->setBcc($bcc);
 
-		$all = $to + $cc + $bcc;
+        $all = $to + $cc + $bcc;
 
-		if (empty($all))
-		{
-			$event->cancelBubble();
-		}
-	}
+        if (empty($all)) {
+            $event->cancelBubble();
+        }
+    }
 
-	/**
-	 * Do nothing
-	 *
-	 * @param Swift_Events_SendEvent $event
-	 */
-	public function sendPerformed(Swift_Events_SendEvent $event)
-	{
-		// Do Nothing
-	}
+    /**
+     * Do nothing.
+     *
+     * @param Swift_Events_SendEvent $event
+     */
+    public function sendPerformed(Swift_Events_SendEvent $event)
+    {
+        // Do Nothing
+    }
 }
-
